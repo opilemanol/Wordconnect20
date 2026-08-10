88b32bbc5da971fc57de1e7e303d0e7cf1522a4f
opilemanol <70860412+opilemanol@users.noreply.github.com>
2026-08-10 16:05:15 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,16 @@
-signingConfigs {
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.wordconnect" // Replace with your actual package name
+    compileSdk = 34
+
+    signingConfigs {
         create("release") {
-            // CHANGE THIS: Change 'my-upload-keybeesharp.jks' to match your new filename
             val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-keywc.jks"
             storeFile = file(keystorePath)
-            
             storePassword = System.getenv("STORE_PASSWORD") ?: project.findProperty("STORE_PASSWORD")?.toString()
             keyAlias = "upload"
             keyPassword = System.getenv("KEY_PASSWORD") ?: project.findProperty("KEY_PASSWORD")?.toString()
@@ -24,3 +31,4 @@ signingConfigs {
             signingConfig = signingConfigs.getByName("release")
         }
     }
+}
