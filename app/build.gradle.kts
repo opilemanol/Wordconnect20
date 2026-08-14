9d67a4bf6ffc8f009286ebabef0e656a9553ba10
opilemanol <70860412+opilemanol@users.noreply.github.com>
2026-08-14 06:02:46 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.aistudio.wordconnectgame.qwpzre"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
